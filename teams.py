e5e3bb5f5b0e4e77e2c46a67c7963893f73936bc
Bossmode248 <you@example.com>
2025-10-07 10:57:51 -0500

    adjusted database

--- teams.py
+++ teams.py
@@ -5,19 +5,20 @@ from typing import List, Tuple
 
 
 
-team_db: List[Tuple[str, int, int, List[str]]] = [
+team_db: List[Tuple[str, int, str, List[str]]] = [
     (
-
+        "los angeles clippers"
 
 
         [
-
+            
 
 
         ]
+
     ),
 
-    
+
 
 
 ]
