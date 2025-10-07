f033ebd68dd3e2a02689336b3841132b5688f3cb
Bossmode248 <you@example.com>
2025-10-07 11:06:51 -0500

    did functions to get things

--- apy1.py
+++ apy1.py
@@ -7,4 +7,17 @@ from match import match
 from typing import List, Tuple, Callable, Any
 
 
-def 
+def get_teamName(teams: Tuple[str, int, str, List[str]]) -> str:
+    return teams[0]
+
+
+def get_coach(teams: Tuple[str, int, str, List[str]]) -> str:
+    return teams[2]
+
+
+def get_yearFounded(teams: Tuple[str, int, str, List[str]]) -> int:
+    return teams[1]
+
+
+def get_topFivePlayers(teams: Tuple[str, int, str, List[str]]) -> List[str]:
+    return teams[3]
