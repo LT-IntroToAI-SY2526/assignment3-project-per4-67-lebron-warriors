b9a6c9e61d4c9cab1b05978d8f2c871c4509cb6d
WenJun Ou <wjunou@cps.edu>
2025-10-08 11:16:12 -0500

    added players

--- teams.py
+++ teams.py
@@ -5,20 +5,141 @@ from typing import List, Tuple
 
 
 
-team_db: List[Tuple[str, int, str, List[str]]] = [
+team_db: List[Tuple[str, int, int, List[str]]] = [
     (
-        "los angeles clippers"
-
-
+        "los angeles clippers",
+        1970 ,
+        "tyronn lae",
         [
-            
+        "kawhi leonard",
+        "james harden",
+        "norman powell",
+        "ivica zubac",
+        "derrick jones jr.",
+
+
+        ]
+    ),
+    (
+        "new york knics",
+        1946 ,
+        "tom thinbadeau",
+        [
+        "jalen brunson",
+        "karl-anthony towns",
+        "mikal bridges",
+        "josh hart",
+        "og anunoby",
 
 
         ]
 
     ),
+    (
+        "boston celtics",
+        1946,
+        "joe muzzulla",
+        [
+        "jayson tatum",
+        "jaylen brown",
+        "jrue holiday",
+        "derrick white",
+        "kristaps porzingis",
+        ]
+    ),
+    (
+        "milwaukee bucks",
+        1968,
+        "dock rivers",
+        [
+        "giannis antetokounmpo",
+        "damian lillard",
+        "khris middleton",
+        "brook lopez",
+        "bobby portis",
+        ]
+    ),
+    (
+        "houston rockets",
+        1967,
+        "ime udoka",
+        [
+        "alperen sengün",
+        "jalen green",
+        "fred vanvleet",
+        "jabari smith jr",
+        "dillon brooks",
+        ]
+    ),
+        (
+        "clevand cavaliers",
+        1976,
+        "kenny atkinson",
+        [
+        "donovan mitchell",
+        "darius garland",
+        "evan mobley",
+        "jarrett allen",
+        "caris leVert",
+        ]
+    ),    (
+        "los angelos lakers",
+        1947,
+        "jj redick",
+        [
+        "LeBron James",
+        "Anthony Davis",
+        "",
+        "",
+        "",
+        ]
+    ),    (
+        "",
+        ,
+        "",
+        [
+        "",
+        "",
+        "",
+        "",
+        "",
+        ]
+    ),    (
+        "",
+          ,
+        "",
+        [
+        "",
+        "",
+        "",
+        "",
+        "",
+        ]
+    ),    (
+      "",
+      ,
+      "",
+        [
+        "",
+        "",
+        "",
+        "",
+        "",
+        ]
+    ),    (
+      "",
+      ,
+      "",
+        [
+        "",
+        "",
+        "",
+        "",
+        "",
+        ]
+    ),
 
-
+    
 
 
 ]
