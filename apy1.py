6bb641738a795590efd40940fb045c41e2ef840a
Michael Stone <Mstone3@cps.edu>
2025-10-08 11:41:30 -0500

    made pa list, query loop and action function.

--- apy1.py
+++ apy1.py
@@ -40,3 +40,85 @@ def coach_by_teamName(match: List[str]) -> list[str]:
         if team == teamName:
             result.append(get_coach(team))
     return result
+
+def teamName_by_yearFounded(matches: List[str]) -> List[str]:
+
+    year = int(matches[0])
+    result = []
+    for teams in team_db:
+        if get_yearFounded(teams) == year:
+            result.append(get_teamName(teams))
+    return result
+
+
+
+
+
+
+
+
+
+
+def bye_action(dummy: List[str]) -> None:
+    raise KeyboardInterrupt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+pa_list: List[Tuple[List[str], Callable[[List[str]], List[Any]]]] = [
+    (str.split("what team was founded in _"), teamName_by_yearFounded),
+    (str.split("who plays on the %"), players_by_teamName),
+    (str.split("who was the coach of %"), coach_by_teamName),
+    (str.split("what movies were directed by %"), title_by_director),
+    (str.split("who acted in %"), actors_by_title),
+    (str.split("when was % made"), year_by_title),
+    (str.split("what years did % direct"), year_by_director),
+    (str.split("in what movies did % appear"), title_by_actor),
+    (["bye"], bye_action),
+]
+
+def search_pa_list(src: List[str]) -> List[str]:
+
+    for pat, act in pa_list:   
+        mat = match(pat, src)
+
+        if mat is not None:
+            answer = act(mat)
+            return answer if answer else ["No answers"]
+    return ["I don't understand"]
+
+
+def query_loop() -> None:
+    """The simple query loop. The try/except structure is to catch Ctrl-C or Ctrl-D
+    characters and exit gracefully.
+    """
+    print("Welcome to the basketball database!\n")
+    while True:
+        try:
+            print()
+            query = input("Your query? ").replace("?", "").lower().split()
+            answers = search_pa_list(query)
+            for ans in answers:
+                print(ans)
+
+        except (KeyboardInterrupt, EOFError):
+            break
+
+    print("\nSo long!\n")
